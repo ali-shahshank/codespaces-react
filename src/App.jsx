3598ceadb9b15d4181207d6dbddf859d7d7995ea
shahshank <74662711+ali-shahshank@users.noreply.github.com>
2026-02-25 07:22:47 +0000

    Minor -/ bug-fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './index.css'
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+<>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/contact' element={<Contact/>} />
